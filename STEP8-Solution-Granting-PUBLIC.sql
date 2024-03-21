524514e25899332770f2b2ea927e9f65a0a8874d
sanderiam <the.philosopher@gmail.com>
2024-03-21 10:31:14 -0500

    fixed semicolon

--- STEP8-Solution-Granting-PUBLIC.sql
+++ STEP8-Solution-Granting-PUBLIC.sql
@@ -1,4 +1,4 @@
-use role ${service_role}
+use role ${service_role};
 grant usage on function "SAMPLE_DATABASE"."SAMPLE_SCHEMA"."SAMPLE_REQUEST_TRANSLATOR"(OBJECT) to role analyst;
 grant usage on function "SAMPLE_DATABASE"."SAMPLE_SCHEMA"."SAMPLE_RESPONSE_TRANSLATOR"(OBJECT) to role analyst;
 grant usage on schema "SAMPLE_DATABASE"."SAMPLE_SCHEMA" to role public;
